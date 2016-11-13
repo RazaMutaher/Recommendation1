6797c280462a17dc8a7b13039bf7895cd530ccdd
RazaMutaher <razamutaher@gmail.com>
2016-11-13 08:00:42 -0800

    Splash Screen Added
    Code Update

---
+++ Movees/app/src/main/java/the/movees/app/FirstSplash.java
@@ -0,0 +1,13 @@
+package the.movees.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FirstSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_splash);
+    }
+}
